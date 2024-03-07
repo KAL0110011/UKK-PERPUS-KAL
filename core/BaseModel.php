e224b32a78ada8d66a511a56053cf5a0fe50e2ef
$_KAL <160807425+KAL0110011@users.noreply.github.com>
2024-03-07 11:56:32 +0700

    Update BaseModel.php

--- core/BaseModel.php
+++ core/BaseModel.php
@@ -6,7 +6,7 @@ class BaseModel
 
 	public function __construct()
 	{
-		$this->mysqli = new mysqli('localhost', 'root', '', 'ukk_perpus_kal'); # sesuaikan dengan nama database masing - masing
+		$this->mysqli = new mysqli('localhost', 'root', '', 'ukk-perpus-kal'); # sesuaikan dengan nama database masing - masing
 	}
 
 	public function getByUsername($username)
