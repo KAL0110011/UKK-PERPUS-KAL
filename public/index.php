d091bdcd614bc7615da47236fde92453b1a534e0
nabila <nabila@gmail.com>
2024-03-18 08:19:59 +0700

    mengububah file index

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@ session_start();
 include '../core/functions.php';
 $url->run();
 
-
+//menanbah user
 // $conn = mysqli_connect('localhost', 'root', '', 'ukk_perpus_kal'); 
 // $password = password_hash('e-mc3', PASSWORD_DEFAULT);
 // mysqli_query($conn, "INSERT INTO users VALUES (null, 'SENKU', '$password', 'doktor@stone.com', 'Ishigami Senku', 'stn', 2);
